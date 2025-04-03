914f1b2ec107ce4c4cf76157705641a0454787e5
Oluwabori Testimony <olucashie@gmail.com>
2025-04-03 02:05:20 -0700

    Add city data endpoint to return city information

--- server/index.js
+++ server/index.js
@@ -9,8 +9,32 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 
+const cities = [
+    {
+        name: "New York",
+        population: 8419000,
+        country: "USA",
+        landmass: "783.8 km²",
+        pictures: ["ny1.jpg", "ny2.jpg"]
+    },
+    {
+        name: "Tokyo",
+        population: 13929000,
+        country: "Japan",
+        landmass: "2,194 km²",
+        pictures: ["tokyo1.jpg", "tokyo2.jpg"]
+    },
+    {
+        name: "Paris",
+        population: 2148000,
+        country: "France",
+        landmass: "105.4 km²",
+        pictures: ["paris1.jpg", "paris2.jpg"]
+    }
+];
+
 app.get('/test', (req, res) => {
-    res.send({name: 'Testimony'})
+    res.send(cities)
 })
 
 app.listen(port, () =>console.log(`Server is running on port ${port}`))
