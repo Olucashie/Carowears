13bbcd99d1ebb8470bc9849fb9e514b9bb5b83f1
Oluwabori Testimony <olucashie@gmail.com>
2025-04-08 01:05:59 -0700

    Implement form submission with validation and connect to MongoDB

--- server/index.js
+++ server/index.js
@@ -2,12 +2,26 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
 dotenv.config()
+const mongoose = require('mongoose');
+
 const port = process.env.PORT || 5000
 const cors = require('cors')
 const bodyParser = require('body-parser')
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
+
+const URI =process.env.uri
+
+
+
+mongoose.connect(URI)
+.then(()=>{
+    console.log('Connected to database successfully')
+})
+.catch((err)=>{
+    console.log(err);
+})
 
 const cities = [
     {
@@ -36,5 +50,16 @@ const cities = [
 app.get('/test', (req, res) => {
     res.send(cities)
 })
+
+app.post('/submit', (req, res)=>{
+
+    console.log(req.body);
+    if (req.body) {
+        res.status(201).json({message: 'form submitted successfully'})
+    } else{
+        res.status(401).json({message: 'form not submitted'})
+    }
+})
+
 
 app.listen(port, () =>console.log(`Server is running on port ${port}`))
